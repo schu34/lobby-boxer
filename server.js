0e533c1fc907226fb53650fc63827be274138f8a
Matthew Schupack <schu34@gmail.com>
2015-07-01 15:27:12 -0500

    added

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+var express = require('express');
+var app = express();
+
+
+var request = require('request');
+
+app.use(express.static(__dirname));
+
+http.createServer(app).listen(3000);
