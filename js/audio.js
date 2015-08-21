14d64e2667389d03cf8ac0319654ce0eea942481
Matthew Schupack <schu34@gmail.com>
2015-08-21 18:24:03 -0500

    factored out playQueue code into new file

--- js/audio.js
+++ js/audio.js
@@ -12,89 +12,27 @@ var pause = function(){
     currentSong.pause();
 };
 
-var playQueue = {
-    queue: [],
-    currentPositionInQueue: -1,
-    song: new Audio(),
-    label: {},
-    playFirst: function(){
-        this.playSong(0);
-    },
-    loadSong: function(queuePos){
-        this.currentPositionInQueue = queuePos;
-        this.song.src = "../audio/" + this.queue[queuePos] + ".mp3";
 
-        this.label.innerText = this.queue[queuePos];
-    },
-    loadSongWithTitle: function(title){
-        this.queue[0] = title;
-    },
-    playSongWithTitle: function(title) {
-        this.reset();
-        this.loadSongWithTitle(title);
-        this.playFirst();
-    },
-    playSong: function(queuePos){
-
-        if(queuePos >= 0 && queuePos < this.queue.length){
-            this.loadSong(queuePos);
-            this.song.currentTime = 0;
-            this.song.play();
-        } else {
-            this.reset();
-        }
-    },
-    playAlbum: function(album){
-        this.reset();
-        this.queue = album.tracklist;
-        this.playFirst();
-    },
-    pause: function(){
-        if(this.song.paused)
-            this.song.play();
-        else
-            this.song.pause();
-    },
-    playNext: function(){
-        var nextNum = this.currentPositionInQueue + 1;
-        if(nextNum === 0 || nextNum > this.queue.length){
-            this.reset();
-        } else {
-            this.playSong(nextNum);
-        }
-    },
-    playPrev: function(){
-        var prevNum = this.currentPositionInQueue - 1;
-
-        if(this.song.currentTime > 2){
-            this.song.currentTime = 0;
-        } else if(prevNum < 0){
-            this.song.pause();
-            this.reset();
-        } else {
-            this.playSong(prevNum);
-        }
-    },
-    reset: function(){
-        this.song.pause();
-        this.currentPositionInQueue = -1;
-        this.queue = [];
-        this.label.innerText = "";
-    }
-};
 
 
 
 window.onload = function(){
+    //array of image html elements. each represents an album cover
     var imgs = [];
     imgs = document.getElementsByClassName('album-cover-image');
     imgs[1].onclick = function(){playQueue.playAlbum(lobbyBoxerEP);};
 
+
+    //set up event handlers for playback control buttons
     document.getElementById('nav-play').onclick = function(){playQueue.pause();};
     document.getElementById('nav-next').onclick = function(){playQueue.playNext();};
     document.getElementById('nav-back').onclick = function(){playQueue.playPrev();};
 
+    //connect label property in playQueue to
     playQueue.label = document.getElementById('song-title');
+
+
+    //set audio element options
     playQueue.song.controls = false;
     playQueue.song.autoplay = false;
     var source = ctx.createMediaElementSource(playQueue.song);
