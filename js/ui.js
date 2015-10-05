65c957a3c491004a8ca2f69364be85efe49b8ef6
Matthew Schupack <schu34@gmail.com>
2015-10-05 12:34:10 -0500

    code cleanup

--- js/ui.js
+++ js/ui.js
@@ -1,11 +1,10 @@
-
 var loadScrollHandlers = function(){
     var navBar = document.getElementById('nav');
 
     window.onscroll = function(){
 
         console.log("scroll event detected");
-        if(window.scrollY > 300){
+        if(window.scrollY > 230){
             navBar.className = "lb-nav-sticky";
         } else {
             navBar.className = "lb-nav";
