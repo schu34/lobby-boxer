16014913e2cda545298fc49efb1349180d7c57d4
Matthew Schupack <schu34@gmail.com>
2016-02-08 17:53:19 -0600

    add bigbucks to albums

--- js/album.js
+++ js/album.js
@@ -17,3 +17,9 @@ var Teddy = new Album("Teddy", 3, teddyTracks);
 var lobbyBoxerTracks = ["A Perfect Future", "The Truman Show", "Headrush", "Cap'n Gown", "Fragile Girl"];
 
 var lobbyBoxerEP = new Album("Lobby Boxer", 5, lobbyBoxerTracks);
+
+var bigBucksTracks = ["Landlocked", "Shine Like Fire", "Sheriff", "Lover's Lane",
+                      "Gaslamp", "I Was Afraid Of Tornados", "Leslie",
+                      "Come Back", "Christopher and Julius", "Where I'll Be next year"];
+
+new Album("Big Bucks", 10, bigBucksTracks);
